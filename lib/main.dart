50ea91e12dc02ad52eb95e255e74e459f9fb50b6
Bullets-jyr <jyr@bullets.co.kr>
2024-07-16 20:55:04 +0900

    Scopes 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final counterProvider = StateProvider<int>(
+  (ref) => 0,
+);
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ProviderScope(
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +19,72 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scope&Lifecyles'),
       ),
+      body: Column(
+        children: [
+          CounterWidget(),
+          ProviderScope(
+            overrides: [
+              counterProvider,
+            ],
+            child: Column(
+              children: [
+                CounterWidget(),
+              ],
+            ),
+          ),
+          ProviderScope(
+            overrides: [
+              counterProvider.overrideWith((ref) => 10),
+            ],
+            child: Column(
+              children: [
+                CounterWidget(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterWidget extends ConsumerWidget {
+  const CounterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final count = ref.watch(counterProvider);
+    return Column(
+      children: [
+        Text('$count'),
+        ElevatedButton(
+          onPressed: () {
+            ref.read(counterProvider.notifier).update((state) => state += 1);
+          },
+          child: Text(
+            '카운트 증가',
+          ),
+        ),
+      ],
     );
   }
 }
